86e4f17c9d4ebb061ef27ded1c9566e3aaf1ab6e
pranavadurai <pranavadurai@gmail.com>
2016-11-27 20:52:07 +0530

    Alarm app using ionic

---
+++ www/js/app.js
@@ -0,0 +1,12 @@
+angular.module('alarm', ['ionic'])
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
